6472b5a7904b7c771ddd9efde91d24606ac9438a
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-23 05:29:03 +0000

    added fields and their getter methods

--- frontier/src/main/java/com/frontier/model/Student.java
+++ frontier/src/main/java/com/frontier/model/Student.java
@@ -1,6 +1,23 @@
 package com.frontier.model;
 
 public class Student {
+    private String fullName, course;
+    private int age;
+
+    // getter methods
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // method that greets the student
     public void greet() {
         System.out.println("Hi");
     }
