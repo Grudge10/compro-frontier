e9fe503921deda8c060e3fcb1f6f0b3446b6b4ad
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-23 05:25:30 +0000

    copied the things sir made

--- frontier/src/main/java/com/frontier/Main.java
+++ frontier/src/main/java/com/frontier/Main.java
@@ -1,7 +1,24 @@
 package com.frontier;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.frontier.model.Student;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student s = new Student();
+        s.greet();
+
+        try {
+            Scanner input = new Scanner(new File("data/student.csv"));
+            while (input.hasNextLine()) {
+                System.out.println(input.nextLine());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ frontier/src/main/java/com/frontier/model/Student.java
@@ -0,0 +1,7 @@
+package com.frontier.model;
+
+public class Student {
+    public void greet() {
+        System.out.println("Hi");
+    }
+}
