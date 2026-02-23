a5e0476bd4bbacc4b8452b767f6de9a801d3b641
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-23 05:26:10 +0000

    closed scanner

--- frontier/src/main/java/com/frontier/Main.java
+++ frontier/src/main/java/com/frontier/Main.java
@@ -17,6 +17,7 @@ public class Main {
                 System.out.println(input.nextLine());
             }
 
+            input.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
